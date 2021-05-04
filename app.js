5428e7c192968722fb599659bd75971b73adb1e1
loji4 <yogawithlindao@gmail.com>
2021-05-04 15:02:21 +1000

    added html and pseudocode

---
+++ app.js
@@ -0,0 +1,14 @@
+// create empty div with clock id 
+
+// create get time function 
+// using JS date object: 
+//  hours 
+//  minutes 
+//  miliseconds 
+//  meridian 
+
+// add them together using interpolation and save to a variable named clocktime 
+
+//render clocktime to screen 
+
+// call get time function 
